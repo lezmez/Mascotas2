f56e32d93b6500ba4dbe1b948ded8308d29158d6
Cristian Lesmes <119712186+lezmez@users.noreply.github.com>
2024-06-03 19:54:46 -0500

    Update registro verificacion para que no se repita el usuario y el correo

--- php/registro_usuario_be.php
+++ php/registro_usuario_be.php
@@ -10,6 +10,32 @@
     $query = "INSERT INTO usuarios(nombre_completo, correo, usuario, contrasena)
               VALUES('$nombre_completo', '$correo', '$usuario', '$contrasena')";
 
+    //verificar que el correo no se repita en la base de datos
+    $verificar_correo = mysqli_query($conexion, "SELECT * FROM usuarios WHERE correo='$correo' ");
+
+    if(mysqli_num_rows($verificar_correo) > 0){
+        echo '
+            <script>
+                alert("Este correo ya esta registrado, intenta con otro diferente");
+                window.location = "../index.php";
+            </script>
+        ';
+        exit();
+    }
+
+     //verificar que el usuario no se repita en la base de datos
+     $verificar_usuario = mysqli_query($conexion, "SELECT * FROM usuarios WHERE usuario='$usuario' ");
+
+     if(mysqli_num_rows($verificar_usuario) > 0){
+         echo '
+             <script>
+                 alert("Este usuario ya esta registrado, intenta con otro diferente");
+                 window.location = "../index.php";
+             </script>
+         ';
+         exit();
+     }
+
     $ejecutar = mysqli_query($conexion, $query);
 
     if($ejecutar){
